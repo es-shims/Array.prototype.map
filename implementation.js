c5e97f72e70f04166c1eda8d67e21f8c0d7ec973
Jordan Harband <ljharb@gmail.com>
2019-09-27 20:44:26 -0700

    implementation

---
+++ implementation.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var ES = require('es-abstract/es2019');
+var bind = require('function-bind');
+var isString = require('is-string');
+
+// Check failure of by-index access of string characters (IE < 9) and failure of `0 in boxedString` (Rhino)
+var boxedString = Object('a');
+var splitString = boxedString[0] !== 'a' || !(0 in boxedString);
+
+var strSplit = bind.call(Function.call, String.prototype.split);
+
+module.exports = function map(callbackfn) {
+	var O = ES.ToObject(this);
+	var self = splitString && isString(O) ? strSplit(O, '') : O;
+	var len = ES.ToUint32(self.length);
+
+	// If no callback function or if callback is not a callable function
+	if (!ES.IsCallable(callbackfn)) {
+		throw new TypeError('Array.prototype.map callback must be a function');
+	}
+
+	var T;
+	if (arguments.length > 1) {
+		T = arguments[1];
+	}
+
+	var A = ES.ArraySpeciesCreate(O, len);
+	var k = 0;
+	while (k < len) {
+		var Pk = ES.ToString(k);
+		var kPresent = ES.HasProperty(O, Pk);
+		if (kPresent) {
+			var kValue = ES.Get(O, Pk);
+			var mappedValue = ES.Call(callbackfn, T, [kValue, k, O]);
+			ES.CreateDataPropertyOrThrow(A, Pk, mappedValue);
+		}
+		k += 1;
+	}
+
+	return A;
+};
---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var define = require('define-properties');
+var ES = require('es-abstract/es6');
+
+var implementation = require('./implementation');
+var getPolyfill = require('./polyfill');
+var polyfill = getPolyfill();
+var shim = require('./shim');
+
+var slice = Array.prototype.slice;
+
+// eslint-disable-next-line no-unused-vars
+var boundMapShim = function map(array, callbackfn) {
+	ES.RequireObjectCoercible(array);
+	return polyfill.apply(array, slice.call(arguments, 1));
+};
+define(boundMapShim, {
+	getPolyfill: getPolyfill,
+	implementation: implementation,
+	shim: shim
+});
+
+module.exports = boundMapShim;
