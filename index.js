07387b4e9af8587977801e6d867725709f5b5da2
Jordan Harband <ljharb@gmail.com>
2024-12-19 21:33:51 -0800

    [Robustness] use callBind.apply

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
 var define = require('define-properties');
 var RequireObjectCoercible = require('es-object-atoms/RequireObjectCoercible');
+var callBind = require('call-bind');
 var callBound = require('call-bound');
 
 var implementation = require('./implementation');
 var getPolyfill = require('./polyfill');
-var polyfill = getPolyfill();
+var polyfill = callBind.apply(getPolyfill());
 var shim = require('./shim');
 
 var $slice = callBound('Array.prototype.slice');
@@ -14,7 +15,7 @@ var $slice = callBound('Array.prototype.slice');
 // eslint-disable-next-line no-unused-vars
 var boundMapShim = function map(array, callbackfn) {
 	RequireObjectCoercible(array);
-	return polyfill.apply(array, $slice(arguments, 1));
+	return polyfill(array, $slice(arguments, 1));
 };
 define(boundMapShim, {
 	getPolyfill: getPolyfill,
